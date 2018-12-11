5e745fb1a8099a8a8080a1072a47535e72d5f983
Partyinlab <39028824+Partyinlab@users.noreply.github.com>
2018-12-11 11:54:54 +0500

    Пара1

--- SomeBot/src/bottt/SomeBot.java
+++ SomeBot/src/bottt/SomeBot.java
@@ -6,52 +6,107 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 
-public class SomeBot extends TelegramLongPollingBot {
-	public static void main(String[] args) {
-		ApiContextInitializer.init();
-		TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
-		try {
-			telegramBotsApi.registerBot(new SomeBot());
-		} catch (TelegramApiException e) {
-			e.printStackTrace();
-		}
-	}
- 
-	@Override
-	public String getBotUsername() {
-		return "ИМЯ_ПОЛЬЗОВАТЕЛЯ_ВАШЕГО_БОТА";
-	}
- 
-	@Override
-	public String getBotToken() {
-		return "ВАШ_ТОКЕН";
-	}
- 
-	@Override
-	public void onUpdateReceived(Update update) {
-		Message message = update.getMessage();
-		if (message != null && message.hasText()) {
-			if (message.getText().equals("/help"))
-				sendMsg(message, "Привет, я робот");
-			else
-				sendMsg(message, "Я не знаю что ответить на это");
-		}
-	}
- 
-	private void sendMsg(Message message, String text) {
-		SendMessage sendMessage = new SendMessage();
-		sendMessage.enableMarkdown(true);
-		sendMessage.setChatId(message.getChatId().toString());
-		sendMessage.setReplyToMessageId(message.getMessageId());
-		sendMessage.setText(text);
-		try {
-			execute(sendMessage);
-		} catch (TelegramApiException e) {
-			e.printStackTrace();
-		}
-	}
- 
+import java.security.Key;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleBot extends TelegramLongPollingBot {
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new SimpleBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "SomeBotOnceToldbot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "735318728:AAEVX_IUvvXWeCLEp7";
+    }
+
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            switch (message.getText()) {
+                case "/start":
+                    sendMsg(message, "");
+                    System.out.println(message.getText());
+                    break;
+                case "Команда 1":
+                    sendMsg(message, " На сегодня");
+                    System.out.println(message.getText());
+                    break;
+                case "Команда 2":
+                    sendMsg(message, " На неделю");
+                    System.out.println(message.getText());
+                    break;
+                default:
+                    sendMsg(message, "Это дефолт! Брейк!");
+                    System.out.println(message.getText());
+                    break;
+            }
+        }
+    }
+
+    public void sendMsg (Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+
+        
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+       
+        List<KeyboardRow> keyboard = new ArrayList<>();
+
+        // Первая строчка
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        
+        keyboardFirstRow.add("Команда 1");
+        keyboardFirstRow.add("Команда 2");
+
+        // Вторая строчка
+        KeyboardRow keyboardSecondRow = new KeyboardRow();
+        
+        keyboardSecondRow.add("Команда 3");
+        keyboardSecondRow.add("Команда 4");
+
+        
+        keyboard.add(keyboardFirstRow);
+        keyboard.add(keyboardSecondRow);
+        
+        replyKeyboardMarkup.setKeyboard(keyboard);
+
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try {
+            sendMessage(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
-
-
